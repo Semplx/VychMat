4dbef30eee3be322217c786a815db4bc8f759db1
Oleg Melnik <botenm@gmail.com>
2013-04-15 00:20:28 +0700

    70% done

--- VychMat/Gauss.cs
+++ VychMat/Gauss.cs
@@ -8,14 +8,71 @@ namespace VychMat
 {
     public static class Gauss
     {
-        public static void Exchange(double[,] arr, int from, int to)
+        static void ForwardWay(double[,] a, double[]b)
         {
-            for(var i = 0; i < arr.Length; i++)
+            double v;
+            var n = b.Length;
+            for (int k = 0, i, j, im; k < n - 1; k++)
             {
-                arr[from, i] = arr[from, i] + arr[to, i];
-                arr[to, i] = arr[from, i] - arr[to, i];
-                arr[from, i] = arr[from, i] - arr[to, i];
+                im = k;
+                for (i = k + 1; i < n; i++)
+                {
+                    if (Math.Abs(a[im,k]) < Math.Abs(a[i,k]))
+                    {
+                        im = i;
+                    }
+                }
+                if (im != k)
+                {
+                    for (j = 0; j < n; j++)
+                    {
+                        v = a[im,j];
+                        a[im,j] = a[k,j];
+                        a[k,j] = v;
+                    }
+                    v = b[im];
+                    b[im] = b[k];
+                    b[k] = v;
+                }
+                for (i = k + 1; i < n; i++)
+                {
+                    v = 1.0 * a[i,k] / a[k,k];
+                    a[i,k] = 0;
+                    b[i] = b[i] - v * b[k];
+                    if (v != 0)
+                        for (j = k + 1; j < n; j++)
+                        {
+                            a[i,j] = a[i,j] - v * a[k,j];
+                        }
+                }
             }
+        }
+
+        static double[] BackwardWay(double[,] a, double[] b)
+        {
+            var n = b.Length;
+            var x = new double[n];
+            double s = 0;
+            x[n - 1] = 1.0 * b[n - 1] / a[n - 1,n - 1];
+            for (int i = n - 2, j; 0 <= i; i--)
+            {
+                s = 0;
+                for (j = i + 1; j < n; j++)
+                {
+                    s = s + a[i,j] * x[j];
+                }
+                x[i] = 1.0 * (b[i] - s) / a[i,i];
+            }
+            return x;
+        }
+
+        public static double[] Solve(double[,] a, double[] b)
+        {
+            var am = (double[,])a.Clone();
+            var bm = (double[])b.Clone();
+            ForwardWay(am, bm);
+            var x = BackwardWay(am, bm);
+            return x;
         }
     }
 }
--- VychMat/MainWindow.xaml.cs
+++ VychMat/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Research.DynamicDataDisplay;
+using DevExpress.Xpf.Charts;
 
 namespace VychMat
 {
@@ -23,10 +25,13 @@ namespace VychMat
         private List<Label> _pointNumberLabels;
         private List<TextBox> _pointXBoxes;
         private List<TextBox> _pointYBoxes;
+        private List<Label> _cLabels; 
         private double?[] _xValues;
         private double?[] _yValues;
         private int _currentMargin = 20;
         private int _number;
+        private const int PlotPointsNumber = 30;
+        private const double PointXStep = 20/PlotPointsNumber;
         public static int PointsNumber { get; set; }
 
         private void PlaceElementGroup()
@@ -45,7 +50,7 @@ namespace VychMat
                          {
                              Margin = new Thickness(_currentMargin, 35, 0, 0),
                              Height = 25,
-                             Width = 25,
+                             Width = 50,
                              HorizontalAlignment = HorizontalAlignment.Left,
                              VerticalAlignment = VerticalAlignment.Top
                          };
@@ -55,13 +60,26 @@ namespace VychMat
                      {
                          Margin = new Thickness(_currentMargin, 70, 0, 0),
                          Height = 25,
-                         Width = 25,
+                         Width = 50,
                          HorizontalAlignment = HorizontalAlignment.Left,
                          VerticalAlignment = VerticalAlignment.Top
                      };
             tb.TextChanged += TextBoxTextChanged;
             _pointYBoxes.Add(tb);
-            _currentMargin += 50;
+            var lb = new Label
+                         {
+                             Margin = new Thickness(_currentMargin-10, 105, 0, 0),
+                             Height = 25,
+                             Width = 75,
+                             HorizontalAlignment = HorizontalAlignment.Left,
+                             VerticalAlignment = VerticalAlignment.Top,
+                             Content="",
+                             //FontSize = 14,
+                             FontWeight = FontWeights.Bold
+
+                         };
+            _cLabels.Add(lb);
+            _currentMargin += 60;
             _number++;
         }
 
@@ -70,6 +88,9 @@ namespace VychMat
             _pointNumberLabels = new List<Label>();
             _pointXBoxes = new List<TextBox>();
             _pointYBoxes = new List<TextBox>();
+            _cLabels = new List<Label>();
+            Chart.Diagram.Series.Add(new LineSeries2D());
+            Chart.Diagram.Series.Add(new LineSeries2D());
         }
 
         private void RemoveAllElements()
@@ -79,6 +100,7 @@ namespace VychMat
                 PointsGrid.Children.Remove(_pointNumberLabels[i]);
                 PointsGrid.Children.Remove(_pointXBoxes[i]);
                 PointsGrid.Children.Remove(_pointYBoxes[i]);
+                PointsGrid.Children.Remove(_cLabels[i]);
             }
         }
 
@@ -100,6 +122,7 @@ namespace VychMat
                 PointsGrid.Children.Add(_pointNumberLabels[i]);
                 PointsGrid.Children.Add(_pointXBoxes[i]);
                 PointsGrid.Children.Add(_pointYBoxes[i]);
+                PointsGrid.Children.Add(_cLabels[i]);
             }
         }
 
@@ -114,6 +137,9 @@ namespace VychMat
 
         private void ApplyButtonClick(object sender, RoutedEventArgs e)
         {
+            //SolutionBlock.Inlines.Clear();
+            Chart.Diagram.Series[0].Points.Clear();
+            Chart.Diagram.Series[1].Points.Clear();
             FindCoefButton.IsEnabled = false;
             try
             {
@@ -139,6 +165,9 @@ namespace VychMat
 
         private void TextBoxTextChanged(object sender, TextChangedEventArgs e)
         {
+            //SolutionBlock.Inlines.Clear();
+            Chart.Diagram.Series[0].Points.Clear();
+            Chart.Diagram.Series[1].Points.Clear();
             try
             {
                 double value;
@@ -153,11 +182,91 @@ namespace VychMat
                     _yValues[i] = value;
                 }
                 FindCoefButton.IsEnabled = true;
+                FindCoefButtonClick(this, e);
+                for(var i = 0; i < PointsNumber; i++)
+                    Chart.Diagram.Series[0].Points.Add(new SeriesPoint((double)_xValues[i], (double)_yValues[i]));
             }
             catch (FormatException exception)
             {
                 FindCoefButton.IsEnabled = false;
+                foreach (var c in _cLabels)
+                    c.Content = "";
+                Chart.Diagram.Series[0].Points.Clear();
+                Chart.Diagram.Series[1].Points.Clear();
             }
+        }
+
+        private void FindCoefButtonClick(object sender, RoutedEventArgs e)
+        {
+            //SolutionBlock.Inlines.Clear();
+            var x = new double[PointsNumber, PointsNumber];
+            var y = new double[PointsNumber];
+            double[] c = null;
+            for(var i = 0; i < PointsNumber; i++)
+            {
+                for(var j = 0; j < PointsNumber; j++)
+                    x[i, j] = Math.Pow((double)_xValues[i], j);
+                y[i] = (double)_yValues[i];
+                c = Gauss.Solve(x, y);
+            }
+            var s = "";
+            for (var i = 0; i < PointsNumber; i++)
+            {
+                c[i] = Math.Round(c[i], 3);
+                s = s + c[i].ToString() + " ";
+            }
+            //var ff = new FontFamily("Palatino Linotype");
+            //var runs = new Run[PointsNumber];
+            //var ssruns = new Run[PointsNumber];
+            for (var i = 0; i < PointsNumber; i++)
+            {
+                //var symb = "";
+                //if (i != PointsNumber - 1 && c[i] >= 0)
+                //    symb = "+";
+                //var str = symb + c[i].ToString() + (i != 0 ? "x" : "");
+                //runs[i] = new Run(str)
+                //                {
+                //                    FontFamily = ff,
+                //                    FontSize = 24
+                //                };
+                //ssruns[i] = new Run(i > 1 ? i.ToString() : "")
+                //                {
+                //                    FontFamily = ff,
+                //                    FontSize = 24
+                //                };
+                //ssruns[i].Typography.Variants = FontVariants.Superscript;
+                _cLabels[i].Content = c[i].ToString();
+                
+
+            }
+            //for(var i = PointsNumber - 1; i >= 0; i--)
+            //{
+            //    SolutionBlock.Inlines.Add(runs[i]);
+            //    SolutionBlock.Inlines.Add(ssruns[i]);
+            //}
+            //MessageBox.Show(s);
+            double px = -10;
+            for(var i = 0; i < 30; i++)
+            {
+                double py = 0;
+                for (var j = 0; j < PointsNumber; j++)
+                {
+                    py = py + c[j]*Math.Pow(px, j);
+                }
+                Chart.Diagram.Series[1].Points.Add(new SeriesPoint(px, py));
+               px = px + 0.666;
+            }
+        }
+
+        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+            for(var i = 0; i < PointsNumber; i++)
+            {
+                _pointXBoxes[i].Text = Math.Round((rnd.NextDouble() * 20 - 10), 2).ToString();
+                _pointYBoxes[i].Text = Math.Round((rnd.NextDouble() * 20 - 10), 2).ToString();
+            }
+            TextBoxTextChanged(this, null);
         }
     }
 }
